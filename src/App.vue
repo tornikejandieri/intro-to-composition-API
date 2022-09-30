79c44fd6cde274ffaac069adc26147931a604733
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-30 13:15:51 +0400

    v-model

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <h3>{{ userAge }}</h3>
     <button @click="setAge">Change Age</button>
     <div>
-      <input type="text" placeholder="First Name" @input="setFirstName" />
-      <input type="text" placeholder="Last Name" @input="setLastName" />
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
     </div>
   </section>
 </template>
@@ -32,19 +32,12 @@ export default {
       uAge.value = 32;
     };
 
-    const setFirstName = (event) => {
-      firstName.value = event.target.value;
-    };
-    const setLastName = (event) => {
-      lastName.value = event.target.value;
-    };
-
     return {
       userName: uName,
       userAge: uAge,
       setAge: setNewAge,
-      setFirstName,
-      setLastName,
+      firstName,
+      lastName,
     };
   },
 
